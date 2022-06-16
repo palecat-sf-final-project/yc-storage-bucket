80d86c5f56994d68085d0086409eb8515e3632db
Viktoiia Vladimirova <viktorija.vladimirova@gmail.com>
2022-06-16 15:10:16 +0200

    create sa and storage bucket

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "folder_id" {
+  description = "The ID of the folder to operate under"
+  type        = string
+}
+
+variable "cloud_id" {
+  description = "The ID of the cloud to operate under"
+  type        = string
+}
+
+variable "auth_token" {
+  description = "Security token or IAM token used for authentication in Yandex.Cloud"
+  type        = string
+}
+
+variable "zone" {
+  description = "Availability zone"
+  type        = string
+  default     = "ru-central1-a"
+}
